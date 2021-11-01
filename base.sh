9841d64f0e50915112affa2a08b1011e8165ad0c
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-01 09:17:20 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -7,10 +7,43 @@ echo "-------------------------------------------------"
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
 sed -i 's/^#Para/Para/' /etc/pacman.conf
+pacman -S --noconfirm reflector
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 echo -e "-----------------------------------------------"
 echo -e "-Setting up $iso mirrors for faster downloads"
 echo -e "-----------------------------------------------"
+
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+
+echo "-------------------------------------------------"
+echo "-------select your disk to format----------------"
+echo "-------------------------------------------------"
+lsblk
+read -p "Please enter disk to work on: (example /dev/sda): " DISK
+echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
+read -p "Are you sure you want to continue (Y/N):" formatdisk
+case $formatdisk in
+
+y|Y|yes|Yes|YES)
+echo "-------------------------------------------------"
+echo -e "\nFormatting disk...\n$HR"
+echo "-------------------------------------------------"
+
+sgdisk -Z ${DISK}
+sgdisk -a 2048 -o ${DISK}
+sgdisk -n 1::+260M --typecode=1:ef00 --change-name=1:'EFI' ${DISK}
+sgdisk -n 2::-0 --typecode=2:8300 --change-name=2:'ROOT' ${DISK}
+
+echo -e "\nCreating Filesystems...\n$HR"
+if [[ ${DISK} =~ "nvme" ]]; then
+  mkfs.fat -F32 -n "EFI" "${DISK}p1"
+  mkfs.btrfs -L "ROOT" "${DISK}p2" -f
+  mount -t btrfs "${DISK}p2" /mnt
+else
+  mkfs.vfat -F32 -n "EFIBOOT" "${DISK}1"
+  mkfs.btrfs -L "ROOT" "${DISK}2" -f
+  mount -t btrfs "${DISK}2" /mnt
+fi
 
 # Setup LUKS key file
 dd bs=512 count=4 if=/dev/random of=/crypto_keyfile.bin iflag=fullblock
