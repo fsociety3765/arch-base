3958f505b011405da029dd5ed9155938c74f7095
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-02 21:51:35 +0000

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -3,7 +3,7 @@
 echo "-------------------------------------------------"
 echo "Installing Paru AUR Helper                       "
 echo "-------------------------------------------------"
-git clone https://aur.archlinux.org/paru.git
-cd paru/;makepkg -si --noconfirm;cd
+git clone https://aur.archlinux.org/paru.git ~/paru
+cd ~/paru/;makepkg -si --noconfirm;cd ~
 rm -rf paru/
 
