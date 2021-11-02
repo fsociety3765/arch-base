39755acb0520cf1f2b946c92b6c3413e82e15400
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-02 16:20:37 +0000

    Create 3-post-setup.sh

---
+++ 3-post-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "-------------------------------------------------"
+echo "Enabling services to start at boot               "
+echo "-------------------------------------------------"
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable firewalld
+systemctl enable acpid
