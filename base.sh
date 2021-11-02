75217b2e6609bc42ea24f5fc33c748599b02d5d4
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-02 18:22:54 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,7 +2,8 @@
 
 bash 0-preinstall.sh
 arch-chroot /mnt /arch-base/1-setup.sh
-#arch-chroot /mnt /usr/bin/runuser -u ${username} -- /home/${username}/arch-base/2-user.sh
+source /mnt/arch-base/install.conf
+arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/arch-base/2-user.sh
 arch-chroot /mnt /arch-base/3-post-setup.sh
 #echo "Rebooting in 3 Seconds ..." && sleep 1
 #echo "Rebooting in 2 Seconds ..." && sleep 1
