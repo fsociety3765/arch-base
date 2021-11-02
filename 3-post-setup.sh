8d5e84f79b43b8b01c1ddd9a14e835fb76bad0d8
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-02 16:21:30 +0000

    Update 3-post-setup.sh

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -12,3 +12,25 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable firewalld
 systemctl enable acpid
+
+echo "-------------------------------------------------"
+echo "Configuring initramfs                            "
+echo "-------------------------------------------------"
+sed -i 's/^MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
+sed -i 's/^FILES=()/FILES=(\/crypto_keyfile.bin)/' /etc/mkinitcpio.conf
+sed -i 's/block filesystems keyboard/block encrypt filesystems keyboard/' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+echo "-------------------------------------------------"
+echo "Configuring Grub                                 "
+echo "-------------------------------------------------"
+ROOT_PARTITION_UUID=$(blkid -o value -s UUID ${ROOT_PARTITION})
+sed -i "s|quiet|cryptdevice=UUID=${ROOT_PARTITION_UUID}:${CRYPTROOT_NAME} root=${CRYPTROOT_PATH}|g" /etc/default/grub
+sed -i 's/^#GRUB_ENABLE_CRYPTODISK/GRUB_ENABLE_CRYPTODISK/' /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "-------------------------------------------------"
+echo "Setting up crypttab                              "
+echo "-------------------------------------------------"
+echo "${CRYPTROOT_NAME}	UUID=${ROOT_PARTITION_UUID}	/crypto_keyfile.bin	luks" > /etc/crypttab
