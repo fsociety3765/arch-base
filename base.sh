6357dea7a75c7a961d244f1a979c2e6c4f01a63e
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 21:13:27 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -7,7 +7,13 @@ arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/arch-base/
 arch-chroot /mnt /arch-base/3-post-setup.sh
 #arch-chroot /mnt -c "rm -rf /arch-base/"
 
-
+echo "-------------------------------------------------"
+echo "Complete                                         "
+echo "Rebooting in 5 seconds...                        "
+echo "Press CTRL+C to cancel the reboot                "
+echo "-------------------------------------------------"
+echo "Rebooting in 5 Seconds ..." && sleep 1
+echo "Rebooting in 4 Seconds ..." && sleep 1
 echo "Rebooting in 3 Seconds ..." && sleep 1
 echo "Rebooting in 2 Seconds ..." && sleep 1
 echo "Rebooting in 1 Second ..." && sleep 1
