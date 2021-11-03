8ec4102cbcffbabc1cba85df21e5a816016d8366
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 23:49:03 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -5,7 +5,7 @@ arch-chroot /mnt /arch-base/1-setup.sh
 source /mnt/arch-base/.env
 arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/arch-base/2-user.sh
 arch-chroot /mnt /arch-base/3-post-setup.sh
-arch-chroot /mnt -c "rm -rf /arch-base/"
+arch-chroot /mnt /bin/bash -c "rm -rf /arch-base/"
 
 echo "-------------------------------------------------"
 echo "Complete                                         "
