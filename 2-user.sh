ff42ea6c89e06c1c6e2581ed643193509ad8ac0c
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 00:26:19 +0000

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -8,3 +8,15 @@ git clone https://aur.archlinux.org/paru.git ~/paru
 cd ~/paru/;makepkg -si --noconfirm;cd ~
 rm -rf paru/
 
+echo "-------------------------------------------------"
+echo "Installing AUR packages                          "
+echo "This may take some time... Please be patient     "
+echo "-------------------------------------------------"
+PKGS=(
+  'timeshift'
+)
+
+for PKG in "${PKGS[@]}"; do
+    paru -S --noconfirm $PKG
+done
+
