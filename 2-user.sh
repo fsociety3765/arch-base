5dd4db094cccc1c0771cbb851f2a0952c4f86b6b
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 17:09:38 +0000

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -4,9 +4,9 @@ echo "-------------------------------------------------"
 echo "Installing Paru AUR Helper                       "
 echo "This may take some time... Please be patient     "
 echo "-------------------------------------------------"
-git clone https://aur.archlinux.org/paru.git ~/paru
-cd ~/paru/;makepkg -si --noconfirm;cd ~
-rm -rf paru/
+git clone https://aur.archlinux.org/paru-bin.git ~/paru-bin
+cd ~/paru-bin/;makepkg -si --noconfirm;cd ~
+rm -rf paru-bin/
 
 echo "-------------------------------------------------"
 echo "Installing AUR packages                          "
@@ -14,6 +14,7 @@ echo "This may take some time... Please be patient     "
 echo "-------------------------------------------------"
 PKGS=(
   'timeshift'
+  'zramd'
 )
 
 for PKG in "${PKGS[@]}"; do
