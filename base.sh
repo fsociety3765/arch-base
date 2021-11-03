eeb5dc0def664fa4bfac41ad11f6461be17ca518
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-03 04:16:15 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -5,6 +5,7 @@ arch-chroot /mnt /arch-base/1-setup.sh
 source /mnt/arch-base/.env
 arch-chroot /mnt /usr/bin/runuser -u ${USERNAME} -- /home/${USERNAME}/arch-base/2-user.sh
 arch-chroot /mnt /arch-base/3-post-setup.sh
+arch-chroot /mnt -c "rm -rf /arch-base/"
 echo "Rebooting in 3 Seconds ..." && sleep 1
 echo "Rebooting in 2 Seconds ..." && sleep 1
 echo "Rebooting in 1 Second ..." && sleep 1
