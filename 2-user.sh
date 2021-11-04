9ac00ff3f311434b1ee0b2a49672f64a08c7d3b0
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-04 01:16:12 +0000

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -13,20 +13,12 @@ echo "Installing AUR packages                          "
 echo "This may take some time... Please be patient     "
 echo "-------------------------------------------------"
 PKGS=(
-  'timeshift'
   'zramd'
 )
 
 for PKG in "${PKGS[@]}"; do
     paru -S --noconfirm $PKG
 done
-
-echo "-------------------------------------------------"
-echo "Setting up Timeshift snapshots                   "
-echo "-------------------------------------------------"
-source /arch-base/.env
-sudo timeshift --create --comment "Initial" --tags D --snapshot-device ${ROOT_PARTITION} --btrfs
-sudo timeshift --list --snapshot-device ${ROOT_PARTITION}
 
 echo "-------------------------------------------------"
 echo "Installing Oh-My-ZSH                             "
