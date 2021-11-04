179e37eedd0a88354ae0726a4dfcbd34dfab293d
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-04 11:49:59 +0000

    Update 3-post-setup.sh

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -51,10 +51,8 @@ cp -r /arch-base/ /home/${USERNAME}/
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/arch-base/
 
 echo "-------------------------------------------------"
-echo "Cleanup                                          "
+echo "Resetting user (${USERNAME}) sudo permissions    "
 echo "-------------------------------------------------"
-sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 echo "${USERNAME} ALL=(ALL) ALL" > "/etc/sudoers.d/${USERNAME}"
 
 echo "-------------------------------------------------"
